77075be51637db07a5242584f9d582ca1cae6b80
vhgauto <victor.gauto@outlook.com>
2024-06-19 11:15:52 -0300

    comentarios en mapa interactivo

--- scripts_quarto/mapa_interactivo.R
+++ scripts_quarto/mapa_interactivo.R
@@ -16,10 +16,12 @@ r_files <- list.files(
   pattern = str_flatten(fechas_v, "|"), 
   full.names = TRUE)
 
+# creo el stack de bandas y agrego las fechas como nombres
 r_list <- map(r_files, raster::stack)
 
 names(r_list) <- fechas_v
 
+# vector de bbox, le quito el nombre a los elementos
 e <- ext(rast(r_list[[1]])) |> 
   vect(crs = "EPSG:32721") |> 
   project("EPSG:4326") |> 
@@ -65,60 +67,29 @@ zoom_lon <- (e[1] + e[2])/2
 zoom_lat <- (e[3] + e[4])/2
 
 # logo GISTAQ
-# logo_img <- "extras/logo_gistaq.png"
+# conviene usar una url en lugar de un archivo local, agrego link a Instagram
 logo_asp <- 1535/538
 logo_ancho <- 100
 logo_alto <- round(logo_ancho/logo_asp) # tienen que ser números enteros
 logo_link <- "https://raw.githubusercontent.com/vhgauto/sameep/main/extras/gistaq_logo.png"
 logo_url <- "https://www.instagram.com/gistaq.utn/"
 
-
-# mapa
-# mapa_int <- leaflet() |> 
-#   # posición y zoom del mapa
-#   setView(lng = zoom_lon, lat = zoom_lat, zoom = 15) |>
-#   # mapa base
-#   addProviderTiles(provider = "OpenStreetMap", group = "OpenStreetMap") |>
-#   # sitios de muestreo
-#   f_circulo(fecha = fechas_v[1]) |>
-#   f_circulo(fecha = fechas_v[2]) |>
-#   f_circulo(fecha = fechas_v[3]) |>
-#   f_circulo(fecha = fechas_v[4]) |>
-#   # RGB
-#   f_rgb(fecha = fechas_v[1]) |>
-#   f_rgb(fecha = fechas_v[2]) |>
-#   f_rgb(fecha = fechas_v[3]) |>
-#   f_rgb(fecha = fechas_v[4]) |>
-#   # control de capas
-#   addLayersControl(
-#     baseGroups = ymd(fechas_v),
-#     options = layersControlOptions(collapsed = FALSE)
-#   ) |> 
-#   # logo
-#   addLogo(
-#     img = logo_link, position = "bottomleft", src = "remote",
-#     width = logo_ancho, height = logo_alto, url = logo_url
-#   ) |> 
-#   # botones
-#   addResetMapButton() |> 
-#   addFullscreenControl(position = "bottomright") |> 
-#   # ocultar capas
-#   showGroup(max(ymd(fechas_v)))
-
+# mapa base
 base <- leaflet() |> 
   # posición y zoom del mapa
   setView(lng = zoom_lon, lat = zoom_lat, zoom = 15) |>
   # mapa base
   addProviderTiles(provider = "OpenStreetMap", group = "OpenStreetMap")
 
-
-for (i in 1:4) {
+# loop que agregar puntos de muestreo y ráster
+for (i in 1:length(fechas_v)) {
   base <- base |> 
     f_circulo(fecha = fechas_v[i]) |> 
     f_rgb(fecha = fechas_v[i])
 }
 
-mapa_int <- base |>
+# mapa
+mapa_interactivo <- base |>
   # control de capas
   addLayersControl(
     baseGroups = ymd(fechas_v),
@@ -132,7 +103,5 @@ mapa_int <- base |>
   # botones
   addResetMapButton() |> 
   addFullscreenControl(position = "bottomright") |> 
-  # ocultar capas
+  # muestro la última fecha al iniciar
   showGroup(max(ymd(fechas_v)))
-
-
