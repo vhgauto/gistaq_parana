cbce673ecb7c42f70f0e82e25313beab3f875b19
vhgauto <victor.gauto@outlook.com>
2024-09-16 15:05:50 -0300

    cambio estilo de firmas espectrales

--- scripts_quarto/figura_evolucion_gis.R
+++ scripts_quarto/figura_evolucion_gis.R
@@ -34,170 +34,95 @@ e <- inner_join(
 
 # figura ------------------------------------------------------------------
 
-g <- ggplot(e, aes(banda, reflect, group = p, color = col)) +
-  geom_line_interactive(
-    aes(data_id = interaction(fecha, p)), hover_nearest = TRUE,
-    linewidth = .6, alpha = .8, show.legend = FALSE) +
-  geom_point(size = 1, shape = 20, color = c3) +
-  geom_point_interactive(
-    aes(data_id = interaction(fecha, p), tooltip = label), size = .7, 
-    shape = 20, hover_nearest = TRUE) +
-  facet_wrap(vars(fecha), ncol = 3, scales = "free_x") +
-  scale_x_discrete(expand = c(0, 0)) +
-  scale_y_continuous(
-    breaks = scales::breaks_pretty(),
-    labels = scales::label_number(decimal.mark = ",", big.mark = ".")
-  ) +
-  scale_color_identity(
-    guide = "legend",
-    labels = c("Orilla\ncorrentina", rep("", 13), "Orilla\nchaqueña")
-  ) +
-  coord_cartesian(clip = "off") +
-  labs(y = NULL, x = NULL, color = NULL) +
-  guides(
-    color = guide_legend(
-      nrow = 1, override.aes = list(size = 6, shape = 15), reverse = TRUE)
-  ) +
-  theme_void(base_size = 6) +
-  theme(
-    aspect.ratio = 1,
-    plot.margin = margin(6, 6, 6, 6),
-    panel.grid.minor = element_blank(),
-    panel.grid.major = element_line(
-      color = c4, linewidth = .06, linetype = 3),
-    panel.spacing = unit(1.1, "line"),
-    axis.title.x = element_text(family = "ubuntu", margin = margin(t = 3)),
-    axis.text = element_text(family = "jet", color = c4),
-    axis.text.y = element_text(hjust = 1, margin = margin(r = 2)),
-    axis.text.x = element_text(margin = margin(t = 2)),
-    panel.background = element_rect(fill = c3),
-    strip.background = element_blank(),
-    strip.text = element_markdown(
-      family = "jet", size = 7, margin = margin(b = 3)),
-    legend.background = element_rect(fill = c6, color = NA),
-    legend.key = element_rect(fill = NA, color = NA),
-    legend.key.size = unit(1, "mm"),
-    legend.position = "bottom",
-    legend.box = "horizontal",
-    legend.text = element_text(
-      vjust = .5, hjust = .5, family = "ubuntu", margin = margin(l = 0),
-      size = 5),
-    legend.text.position = "bottom",
-    legend.key.width = unit(1, "mm"),
-    legend.key.spacing.x = unit(-4.8, "mm")
-  )
-
-figura_evolucion_gis <- girafe(
-  ggobj = g,
-  bg = c6,
-  options = list(
-    opts_hover(
-      css = girafe_css(css = "")
-    ),
-    opts_tooltip(
-      opacity = 1,
-      css = glue(
-        "color:{c1};padding:5px;font-family:JetBrains Mono;",
-        "border-style:solid;border-color:{c4};background:{c3}"),
-      use_cursor_pos = TRUE,
-      offx = 5,
-      offy = 5),
-    opts_sizing(width = 1, rescale = TRUE),
-    opts_hover_inv(css = "opacity:.4"),
-    opts_toolbar(saveaspng = FALSE)
-  )
-)
-
-###
-
 f_firma_espectral <- function(x) {
 
   d <- e |> 
     filter(fecha == x)
 
-fig_evo_gis_label <- c("Chaco", rep("", length(unique(d$p))-2), "Corrientes")
+  fig_evo_gis_label <- c("Chaco", rep("", length(unique(d$p))-2), "Corrientes")
 
-colores_gis <- colorRampPalette(colors = c(c1, c4, c2))(length(unique(d$p)))
+  colores_gis <- colorRampPalette(colors = c(c1, c4, c2))(length(unique(d$p)))
 
-midpoint_gis <- median(unique(d$p))
+  midpoint_gis <- median(unique(d$p))
 
-g <- ggplot(d, aes(banda, reflect, group = p, color = p, fill = p)) +
-  geom_line_interactive(
-    aes(data_id = interaction(fecha, p)), hover_nearest = TRUE,
-    linewidth = 1, alpha = .8, show.legend = FALSE) +
-  geom_point(size = 1, shape = 21, color = c3, stroke = .6) +
-  scale_x_discrete(expand = c(0, 0)) +
-  scale_y_continuous(
-    breaks = scales::breaks_pretty(),
-    labels = scales::label_number(decimal.mark = ",", big.mark = ".")
-  ) +
-  scale_fill_gradient2(
+  g <- ggplot(d, aes(banda, reflect, group = p, color = p, fill = p)) +
+    geom_line_interactive(
+      aes(data_id = interaction(fecha, p)), hover_nearest = TRUE,
+      linewidth = 1, alpha = .8, show.legend = FALSE) +
+    geom_point_interactive(
+      aes(data_id = interaction(fecha, p)), size = .7, shape = 21) +
+    scale_x_discrete(expand = c(0, 0)) +
+    scale_y_continuous(
+      breaks = scales::breaks_pretty(),
+      labels = scales::label_number(decimal.mark = ",", big.mark = ".")
+    ) +
+    scale_fill_gradient2(
+      low = c1, mid = c4, high = c2, midpoint = midpoint_gis,
+      breaks = unique(d$p),
+      labels = fig_evo_gis_label) +
+    scale_color_gradient2(
     low = c1, mid = c4, high = c2, midpoint = midpoint_gis,
     breaks = unique(d$p),
-    labels = fig_evo_gis_label) +
-  scale_color_gradient2(
-  low = c1, mid = c4, high = c2, midpoint = midpoint_gis,
-  breaks = unique(d$p),
-  labels = fig_evo_gis_label
-) +
-  coord_cartesian(clip = "off") +
-  labs(y = NULL, x = NULL, color = NULL, fill = NULL) +
-  guides(
-    fill = guide_colorbar(
-      nrow = 1, 
-      override.aes = list(size = 16, shape = 15, color = colores_gis),
-      reverse = FALSE),
-    color = guide_none()
+    labels = fig_evo_gis_label
   ) +
-  theme_void(base_size = 11) +
-  theme(
-    aspect.ratio = 1,
-    plot.margin = margin(6, 6, 6, 6),
-    panel.grid.minor = element_blank(),
-    panel.grid.major = element_line(
-      color = c4, linewidth = .06, linetype = 3),
-    panel.spacing = unit(1.1, "line"),
-    axis.title.x = element_text(family = "ubuntu", margin = margin(t = 3)),
-    axis.text = element_text(family = "jet", color = c7),
-    axis.text.y = element_text(hjust = 1, margin = margin(r = 2)),
-    axis.text.x = element_text(margin = margin(t = 2)),
-    axis.ticks = element_line(color = c7),
-    axis.ticks.length = unit(1, "mm"),
-    panel.background = element_rect(fill = c3),
-    strip.background = element_blank(),
-    strip.text = element_markdown(
-      family = "jet", size = 7, margin = margin(b = 3)),
-    legend.background = element_rect(fill = c6, color = NA),
-    legend.key = element_rect(fill = NA, color = NA),
-    legend.key.size = unit(1, "mm"),
-    legend.position = "top",
-    legend.box = "horizontal",
-    legend.text = element_text(
-      vjust = .5, hjust = .5, family = "ubuntu", margin = margin(t = 3),
-      size = 11),
-    legend.text.position = "bottom",
-    legend.key.width = unit(10, "mm"),
-    legend.key.height = unit(2, "mm")
-  )
+    coord_cartesian(clip = "off") +
+    labs(y = NULL, x = NULL, color = NULL, fill = NULL) +
+    guides(
+      fill = guide_colorbar(
+        nrow = 1, 
+        override.aes = list(size = 16, shape = 15, color = colores_gis),
+        reverse = FALSE),
+      color = guide_none()
+    ) +
+    theme_void(base_size = 11) +
+    theme(
+      aspect.ratio = 1,
+      plot.margin = margin(6, 6, 6, 6),
+      panel.grid.minor = element_blank(),
+      panel.grid.major = element_line(
+        color = c4, linewidth = .06, linetype = 3),
+      panel.spacing = unit(1.1, "line"),
+      axis.title.x = element_text(family = "ubuntu", margin = margin(t = 3)),
+      axis.text = element_text(family = "jet", color = c7),
+      axis.text.y = element_text(hjust = 1, margin = margin(r = 2)),
+      axis.text.x = element_text(margin = margin(t = 2)),
+      axis.ticks = element_line(color = c7),
+      axis.ticks.length = unit(1, "mm"),
+      panel.background = element_rect(fill = c3),
+      strip.background = element_blank(),
+      strip.text = element_markdown(
+        family = "jet", size = 7, margin = margin(b = 3)),
+      legend.background = element_rect(fill = c6, color = NA),
+      legend.key = element_rect(fill = NA, color = NA),
+      legend.key.size = unit(1, "mm"),
+      legend.position = "top",
+      legend.box = "horizontal",
+      legend.text = element_text(
+        vjust = .5, hjust = .5, family = "ubuntu", margin = margin(t = 3),
+        size = 11),
+      legend.text.position = "bottom",
+      legend.key.width = unit(10, "mm"),
+      legend.key.height = unit(2, "mm")
+    )
 
   figura_evolucion_gis <- girafe(
-  ggobj = g,
-  bg = c6,
-  options = list(
-    opts_hover(
-      css = girafe_css(css = "")
-    ),
-    opts_tooltip(
-      opacity = 1,
-      css = glue(
-        "color:{c1};padding:5px;font-family:JetBrains Mono;",
-        "border-style:solid;border-color:{c4};background:{c3}"),
-      use_cursor_pos = TRUE,
-      offx = 5,
-      offy = 5),
-    opts_sizing(width = 1, rescale = TRUE),
-    opts_hover_inv(css = "opacity:.4"),
-    opts_toolbar(saveaspng = FALSE)
+    ggobj = g,
+    bg = c6,
+    options = list(
+      opts_hover(
+        css = girafe_css(css = "")
+      ),
+      opts_tooltip(
+        opacity = 1,
+        css = glue(
+          "color:{c1};padding:5px;font-family:JetBrains Mono;",
+          "border-style:solid;border-color:{c4};background:{c3}"),
+        use_cursor_pos = TRUE,
+        offx = 5,
+        offy = 5),
+      opts_sizing(width = 1, rescale = TRUE),
+      opts_hover_inv(css = "opacity:.2"),
+      opts_toolbar(saveaspng = FALSE)
   )
 
 )
@@ -209,4 +134,3 @@ g <- ggplot(d, aes(banda, reflect, group = p, color = p, fill = p)) +
 fechas_gis_v <- rev(sort(unique(e$fecha)))
 
 lista_figura_evolucion_gis <- map(fechas_gis_v, f_firma_espectral)
-
