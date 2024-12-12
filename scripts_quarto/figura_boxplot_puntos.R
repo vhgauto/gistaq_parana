120cda0233f036bb098beb8795e637c4cabd4790
Víctor Gauto <victor.gauto@outlook.com>
2024-12-12 20:20:23 -0300

    agrego significancia entre orillas chaqueña y correntina, por banda

--- scripts_quarto/figura_boxplot_puntos.R
+++ scripts_quarto/figura_boxplot_puntos.R
@@ -1,5 +1,31 @@
 #
 
+# función -----------------------------------------------------------------
+
+# función que verifica la diferencia entre reflectancias por banda espectral
+# y sitio
+f_puntos <- function(df, p_i, p_f) {
+  p <- df |> 
+    filter(punto %in% c(p_i, p_f)) |> 
+    pivot_wider(
+      id_cols = fecha,
+      names_from = punto,
+      values_from = reflect
+    ) |>
+    unnest(everything()) |> 
+    rename(
+      "p_inicial" = 2,
+      "p_final" = 3
+    )
+  
+  wilcox.test(p$p_inicial, p$p_final, paired = FALSE, exact = TRUE) |>
+    broom::tidy() |>
+    select(p.value) |>
+    mutate(
+      es_significativo = p.value < .05
+    )
+}
+
 # datos ------------------------------------------------------------------
 
 # 3 puntos para extraer la reflectancia
@@ -38,14 +64,38 @@ e_tbl <- map(lista_r3, ~terra::extract(.x, puntos, xy = TRUE)) |>
   mutate(reflect = reflect/10000) |>
   select(-ID)
 
+# obtengo la significancia en los diferentes puntos, por banda
+p_tbl <- e_tbl |> 
+  nest(.by = banda) |> 
+  mutate(
+    P1P3 = map(data, ~f_puntos(.x, "P1", "P3"))
+  ) |> 
+  select(-data) |> 
+  pivot_longer(
+    cols = -banda
+  ) |> 
+  unnest(value) |> 
+  mutate(
+    x = 1,
+    xend = 3,
+    y = .3,
+    yend = .3
+  ) |> 
+  mutate(
+    label = if_else(es_significativo, simbolo_sig, NA)
+  )
+
 g <- e_tbl |>
-  filter(fecha != ymd(20231212)) |>
   ggplot(aes(punto, reflect, group = x, fill = punto)) +
   geom_boxplot(
     linewidth = .2, outlier.size = .3, key_glyph = draw_key_point,
     outlier.color = c9, width = .8
   ) +
-  facet_wrap(vars(banda), scales = "free", nrow = 3) +
+  geom_richtext(
+    data = p_tbl, aes(Inf, Inf, label = label), inherit.aes = FALSE, fill = c11,
+    size = 4, label.color = NA, hjust = 1, vjust = 1
+  ) +
+  facet_wrap(vars(banda), scales = "free", nrow = 4) +
   scale_y_continuous(
     breaks = seq(.1, .3, .05),
     limits = c(.1, .3),
@@ -61,14 +111,14 @@ g <- e_tbl |>
   labs(x = NULL, y = "R<sub>rs</sub>", color = "Sitio") +
   guides(
     fill = guide_legend(
-      override.aes = list(color = c7, shape = 22, size = 3, stroke = .3)
+      override.aes = list(color = c7, shape = 22, size = 5, stroke = .3)
     )
   ) +
-  theme_minimal(base_size = 7) +
+  theme_minimal(base_size = 9) +
   theme(
     aspect.ratio = 1,
     plot.margin = margin(r = 1, l = 1, t = 0, b = 0),
-    panel.background = element_rect(fill = c10, color = NA),
+    panel.background = element_rect(fill = c11, color = NA),
     panel.spacing.x = unit(1.6, "line"),
     panel.spacing.y = unit(1, "line"),
     panel.grid.major.x = element_blank(),
@@ -96,6 +146,6 @@ ggsave(
   plot = g,
   filename = "figuras/puntos_boxplot.png",
   width = 20,
-  height = 14.8,
+  height = 25,
   units = "cm"
 )
