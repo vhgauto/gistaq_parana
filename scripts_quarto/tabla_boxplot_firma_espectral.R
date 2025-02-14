7b6230cf4963c145cba448c03cda70f83f8c453c
Víctor Gauto <victor.gauto@outlook.com>
2025-02-14 15:53:46 -0300

    corrijo lectura de archivos ráster, cambio tabla por figura

--- scripts_quarto/tabla_boxplot_firma_espectral.R
+++ scripts_quarto/tabla_boxplot_firma_espectral.R
@@ -1,5 +1,3 @@
-#
-source("scripts_quarto/soporte.R")
 
 # datos ------------------------------------------------------------------
 
@@ -8,6 +6,7 @@ v <- vect("vector/transecta.gpkg")
 
 # ráster de recortes
 archivos_r <- list.files("recorte/", pattern = "tif$", full.names = TRUE)
+archivos_r <- archivos_r[!str_detect(archivos_r, "rsi")]
 fechas_r <- str_remove(basename(archivos_r), ".tif")
 
 lista_r <- map(
@@ -41,29 +40,38 @@ e_tbl <- map(lista_r3, ~terra::extract(.x, v, xy = TRUE)) |>
   ) |>
   mutate(banda = factor(banda, levels = orden_bandas))
 
-tabla_boxplot_firma_espectral <- e_tbl |>
-  filter(fecha != ymd(20231212)) |>
-  group_by(banda) |>
-  summarise(
-    r = list(reflect),
-    .groups = "drop"
-  ) |>
-  gt() |>
-  gtExtras::gt_plt_dist(
-    column = r,
-    type = "boxplot",
-    line_color = c1,
-    fill_color = c3
-  ) |>
-  cols_label(
-    banda = "Banda",
-    r = md("Distribución R<sub>rs</sub>")
-  ) |>
-  tab_style(
-    locations = cells_body(columns = "banda"),
-    style = cell_text(weight = "bold")
-  ) |>
-  tab_style(
-    locations = cells_column_labels(),
-    style = cell_text(v_align = "top", weight = "bold")
+g <- e_tbl |>
+  mutate(banda = fct_rev(banda)) |>
+  ggplot(aes(reflect, banda)) +
+  geom_boxplot(
+    color = c1, linewidth = .3, outlier.alpha = .4, outlier.color = c2,
+    fill = c11
+  ) +
+  scale_x_continuous(
+    breaks = scales::breaks_width(.1),
+    labels = scales::label_number(big.mark = ".", decimal.mark = ",")
+  ) +
+  coord_cartesian(xlim = c(0, .45), expand = FALSE, clip = "off") +
+  labs(x = "R<sub>rs</sub>") +
+  theme_void() +
+  theme(
+    plot.margin = margin(t = 3, b = 3),
+    axis.title.x = element_markdown(family = "ubuntu", margin = margin(t = 3)),
+    axis.text.x = element_text(
+      family = "jet", margin = margin(t = 10), size = 9
+    ),
+    axis.text.y = element_text(
+      family = "Ubuntu", face = "bold", margin = margin(r = 3), size = 9
+    ),
+    panel.grid.major.x = element_line(
+      color = c9, linewidth = .1, linetype = "FF"
+    )
   )
+
+ggsave(
+  plot = g,
+  filename = "figuras/boxplot_reflect.png",
+  width = 10,
+  height = 14,
+  units = "cm"
+)
